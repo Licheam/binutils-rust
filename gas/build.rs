74dc8a35367cec60aae388da7b8a0493e35e88d1
Leachim <32847549+Licheam@users.noreply.github.com>
2024-10-30 13:39:58 +0800

    update binutils

--- gas/build.rs
+++ gas/build.rs
@@ -1,7 +1,7 @@
 #[cfg(all(unix, not(target_os = "macos")))]
 fn main() {
     // add unix dependencies below
-     println!("cargo:rustc-flags=-L../opcodes -l opcodes");
+     println!("cargo:rustc-flags=-L./opcodes -l opcodes");
     // println!("cargo:rustc-flags=-l readline");
 }
 
